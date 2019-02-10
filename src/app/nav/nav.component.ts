04a73f77a9f54e81cb3d399e9d44055966ea8162
justisGipson <justis710@gmail.com>
2019-02-10 13:12:51 -0500

    removed useless code from nav component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,20 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  showAbout: boolean = false;
-
   title: string = 'Star Wars API Search';
-
-  public show: boolean = false;
-  public buttonName: any = true;
-
-  toggle() {
-    this.show = !this.show;
-    if(this.show)
-      this.buttonName = false;
-    else
-      this.buttonName = true;
-}
 
   constructor() { }
 
