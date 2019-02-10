c00e0f167d6e6d1750f2813073b27f1fe6dd708a
justisGipson <justis710@gmail.com>
2019-02-10 15:17:13 -0500

    updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule  } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { NavComponent } from './nav/nav.component';
 import { AboutComponent } from './about/about.component';
-
-import { AppComponent } from './app.component';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { SwapiService } from './services/swapi.service'; 
 
 
 @NgModule({
@@ -25,7 +26,7 @@ import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [FormBuilder],
+  providers: [SwapiService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
